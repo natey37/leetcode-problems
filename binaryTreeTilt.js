e759813e2d6b928d0aa512cd10c56622a53dcad2
Nathan Brickett <natey37@gmail.com>
2020-05-12 17:20:25 -0400

    binary tree tilt

---
+++ binaryTreeTilt.js
@@ -0,0 +1,46 @@
+/*
+563. Binary Tree Tilt
+
+Given a binary tree, return the tilt of the whole tree.
+
+The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and the sum of all right subtree node values. Null node has tilt 0.
+
+The tilt of the whole tree is defined as the sum of all nodes' tilt.
+
+Example:
+Input: 
+         1
+       /   \
+      2     3
+Output: 1
+Explanation: 
+Tilt of node 2 : 0
+Tilt of node 3 : 0
+Tilt of node 1 : |2-3| = 1
+Tilt of binary tree : 0 + 0 + 1 = 1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/*
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findTilt = function(root) {
+    let tiltSum = 0 
+    const recurr = (root) => { 
+        if(!root) return 0
+        let leftSum = recurr(root.left)
+        let rightSum = recurr(root.right)  
+        tiltSum += Math.abs(leftSum - rightSum)
+        return root.val + leftSum + rightSum
+    }
+    recurr(root)
+    return tiltSum 
+};
