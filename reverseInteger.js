848beaab43800d13f0d90c15a700ba4b4a901cf8
Nathan Brickett <natey37@gmail.com>
2020-08-10 12:47:31 -0400

    reverse Integer

--- reverseInteger.js
+++ reverseInteger.js
@@ -30,3 +30,27 @@ var reverse = function(x) {
     }
     return x>0? num : num*-1
 };
+
+var reverse = function(x) {
+    let negative = x < 0 ? true : false 
+    let num = Math.abs(x).toString().split('')
+    let front = 0
+    let back = num.length - 1
+    while(front < back){
+        let temp = num[front]
+        num[front] = num[back]
+        num[back] = temp 
+        front++
+        back--
+    }
+    for(const number in num){
+        console.log(typeof number)
+        console.log(num[number])
+        if(num[number] === "0") num[number] = undefined
+        if(num[number] !== "0") break;
+    }
+    let filtered = num.filter(num => num !== undefined)
+    if(parseInt(filtered.join('')) > 2147483647) return 0
+
+    return negative ? `-${filtered.join('')}` : filtered.join('')
+};
