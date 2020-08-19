4d205cea56fdff6c27aed172ae7e4fe5e0589ecc
Nathan Brickett <natey37@gmail.com>
2020-08-19 18:49:55 -0400

    implement trie

---
+++ impletentTrie.js
@@ -0,0 +1,64 @@
+/*
+
+208. Implement Trie (Prefix Tree)
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");   
+trie.search("app");     // returns true
+*/
+
+
+/**
+ * Initialize your data structure here.
+ */
+var Trie = function() {
+    this.words = []
+};
+
+/**
+ * Inserts a word into the trie. 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    this.words.push(word)
+};
+
+/**
+ * Returns if the word is in the trie. 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    if(this.words.includes(word)) return true
+    return false
+};
+
+/**
+ * Returns if there is any word in the trie that starts with the given prefix. 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    for(let i=0;i<this.words.length;i++){
+        let begin = this.words[i].slice(0,prefix.length)
+        if(begin === prefix) return true
+    }
+    return false
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
