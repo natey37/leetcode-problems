d730a72845a47db5fe5d3990d8ff539f83897a71
Nathan Brickett <natey37@gmail.com>
2020-08-26 16:40:44 -0400

    additional solution for add two numbers

--- addTwoNumbers1.js
+++ addTwoNumbers1.js
@@ -25,6 +25,25 @@ Explanation: 342 + 465 = 807.
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
+
+    //optimal solution 
+        let dummyHead = new ListNode(0)
+        let curr = dummyHead
+        let carry = 0
+        while(l1 || l2){
+            let x = l1 ? l1.val : 0
+            let y = l2 ? l2.val : 0
+            let sum = x + y + carry 
+            carry = Math.floor(sum / 10)
+            curr.next = new ListNode(sum % 10)
+            curr = curr.next
+            if(l1) l1 = l1.next
+            if(l2) l2 = l2.next
+        }
+        if(carry === 1) curr.next = new ListNode(carry)
+        return dummyHead.next
+   
+
   //Decided to take a different approach, obviously takes up extra memory, but relatively efficient time wise. 
 
   //seperate both linked lists into arrays
