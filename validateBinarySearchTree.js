094c43d5dd911ab9bce44b9fd6c95e5b1de1c34a
Nathan Brickett <natey37@gmail.com>
2020-08-26 15:39:36 -0400

    validate binary search tree

---
+++ validateBinarySearchTree.js
@@ -0,0 +1,65 @@
+/*
+98. Validate Binary Search Tree
+
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+    2
+   / \
+  1   3
+
+Input: [2,1,3]
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/*
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    let inOrder = []
+   
+    //if we are looking for a valid binary search tree than we know that if do an inorder traversal of our tree every node we visit should be in order (correctly sorted)
+   const order = (node) => {
+       if(node == null) return 
+       
+       order(node.left)
+       inOrder.push(node.val)
+       order(node.right)
+   }
+   order(root)
+   
+   //once we have our array of our inorder traversal we loop through to make sure it is sorted
+   for(let i=1;i<=inOrder.length - 1;i++){
+       if(inOrder[i] <= inOrder[i-1] || inOrder[i] >= inOrder[i+1]) return false
+   }
+   
+   return true
+};
