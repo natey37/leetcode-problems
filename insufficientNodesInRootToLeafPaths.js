befb2d0f94d403212bdeec67ad07403dcb3a5529
Nathan Brickett <natey37@gmail.com>
2020-09-22 14:16:05 -0700

    insufficient nodes in root to leaf paths

---
+++ insufficientNodesInRootToLeafPaths.js
@@ -0,0 +1,60 @@
+/*
+1080. Insufficient Nodes in Root to Leaf Paths
+Given the root of a binary tree, consider all root to leaf paths: paths from the root to any leaf.  (A leaf is a node with no children.)
+
+A node is insufficient if every such root to leaf path intersecting this node has sum strictly less than limit.
+
+Delete all insufficient nodes simultaneously, and return the root of the resulting binary tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,-99,-99,7,8,9,-99,-99,12,13,-99,14], limit = 1
+
+Output: [1,2,3,4,null,null,7,8,9,null,14]
+Example 2:
+
+
+Input: root = [5,4,8,11,null,17,4,7,1,null,null,5,3], limit = 22
+
+Output: [5,4,8,11,null,17,4,7,null,null,null,5]
+ 
+
+Example 3:
+
+
+Input: root = [1,2,-3,-5,null,4,null], limit = -1
+
+Output: [1,null,-3,4]
+ 
+*/
+
+
+/*
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/*
+ * @param {TreeNode} root
+ * @param {number} limit
+ * @return {TreeNode}
+ */
+var sufficientSubset = function(root, limit) {
+    limit -= root.val
+    if(!root.left && !root.right) return limit <= 0 ? root : null
+    
+    if(root.left){
+        root.left = sufficientSubset(root.left, limit)
+    }
+    if(root.right){
+        root.right = sufficientSubset(root.right, limit)
+    }
+    return root.right == root.left ? null : root
+
+};
