b1d9a0a218c9468531ff9610b2520ba590141109
Nathan Brickett <natey37@gmail.com>
2020-09-29 17:47:12 -0700

    invert binary tree

---
+++ invertBinaryTree.js
@@ -0,0 +1,49 @@
+/*
+226. Invert Binary Tree
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/*
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    const traverse = (node) => {
+        if(root === null) return 
+        let temp = node.left 
+        node.left = node.right
+        node.right = temp 
+
+        invertTree(node.left)
+        invertTree(node.right)
+    }
+    
+    traverse(root)
+    return root
+};
