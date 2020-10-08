9327519bc38b02fcfe0b701b7f9e23e97f8f6ff7
Nathan Brickett <natey37@gmail.com>
2020-10-08 13:57:52 -0700

    find itinerary

---
+++ reconstructItinerary.js
@@ -0,0 +1,57 @@
+/*
+332. Reconstruct Itinerary
+Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
+
+Note:
+
+If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string. For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+All airports are represented by three capital letters (IATA code).
+You may assume all tickets form at least one valid itinerary.
+One must use all the tickets once and only once.
+Example 1:
+
+Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+Example 2:
+
+Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+             But it is larger in lexical order.
+*/
+
+
+/*
+ * @param {string[][]} tickets
+ * @return {string[]}
+ */
+var findItinerary = function(tickets) {
+    let map = {}
+    
+    for(let i=0;i<tickets.length;i++){
+        let depart = tickets[i][0]
+        let arrive = tickets[i][1]
+        
+        if(!map[depart]){
+            map[depart] = [arrive]
+        } else {
+            map[depart].push(arrive)
+        }
+    }
+
+    for(let location in map){
+        map[location].sort()
+    }
+    
+    let itinerary = []
+    const dfs = (node) => {
+        let dest = map[node]
+        while(dest && dest.length > 0){
+            dfs(dest.shift())
+        }
+        itinerary.push(node)
+    }
+    
+    dfs("JFK")
+    return itinerary.reverse()
+}
