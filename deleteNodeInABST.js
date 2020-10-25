f883192d3d02f58ad5bfe36ef397b07cc6a8b007
Nathan Brickett <natey37@gmail.com>
2020-10-25 14:09:22 -0700

    add example for delete node in a bst

--- deleteNodeInABST.js
+++ deleteNodeInABST.js
@@ -36,13 +36,24 @@ var deleteNode = function(root, key) {
     const dl = (root, key) => {
         if(!root) return root
         
+        //first we can narrow our search right away by checking if the nodes value is greater than the key, if it is than because we are working with a BST we know that it can only be on the left side of the tree, so we call our dl function on node.left
         if(root.val > key){
             root.left = dl(root.left, key)
         }
+        //again same logic, here check is the nodes value is less than the key, if it is than it can only be on the right side of the tree
         if(root.val < key){
             root.right = dl(root.right, key)
         }
         
+        //if we find a node that has a value equal to key than we have a few options
+        //if the node is a leaf (meaning it has no left or right nodes) than we can set it equal to null 
+        //if the node doesnt have a node.left but does have a node.right then set our node equal to the right node
+        //again same but opposite logic, if the node doesnt have a node.right but does have a node.left then set our node equal to the left node
+        //lastly if none of the above than we do a couple things
+            //Because we need to keep the correct structure of a BST, we will need to find the minimum of the subtree beneath our deleted node. 
+            //We will use our helper function getMin which will find the smallest node in the substree
+            //then we set our nodes value equal to smallest node value we found from our helper function
+            //then we will set the node.right equal to our dl function with the parameters of our node.right 
         if(root.val === key){
             if(!root.left && !root.right){
                 root = null
@@ -53,14 +64,13 @@ var deleteNode = function(root, key) {
             } else {
                 let node = getMin(root.right)
                 root.val = node.val
-                root.right = deleteNode(root.right, node.val)
+                root.right = dl(root.right, node.val)
             }
         }
         return root
     }
     
     return dl(root, key)
-    
     
 };
 
@@ -71,3 +81,36 @@ const getMin = (node) => {
     }
     return current
 }
+
+
+/*
+Input: root = [5,3,6,2,4,null,7], key = 3
+
+dl([5,3,6,2,4,null,7], 3)
+    if(root.val > 3){
+        root.left = dl([[3,2,4]], 3)
+    }
+
+    `   dl([[3,2,4]], 3)
+                if(root.val === key)
+                else {
+                    node = getMin([4])
+                }
+
+                    getMin([4])
+                        current = [4]
+                        while(current.left) -> doesnt run
+
+                        return current
+
+                    node = [4]
+                    root.val = 4
+                `   root.right = dl(root.right, node.val)
+
+                        dl(null, 4)
+                            if(!node) return root
+                    
+                    root.right = null
+
+                return root -> [5,4,6,2,null,null,7]
+*/
