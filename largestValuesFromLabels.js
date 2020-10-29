91d0f6847bc5bed481d1135635d9094c973a2d53
Nathan Brickett <natey37@gmail.com>
2020-10-29 13:54:37 -0700

    largest values from labels

---
+++ largestValuesFromLabels.js
@@ -0,0 +1,60 @@
+/*
+1090. Largest Values From Labels
+We have a set of items: the i-th item has value values[i] and label labels[i].
+
+Then, we choose a subset S of these items, such that:
+
+|S| <= num_wanted
+For every label L, the number of items in S with label L is <= use_limit.
+Return the largest possible sum of the subset S.
+
+ 
+
+Example 1:
+
+Input: values = [5,4,3,2,1], labels = [1,1,2,2,3], num_wanted = 3, use_limit = 1
+Output: 9
+Explanation: The subset chosen is the first, third, and fifth item.
+Example 2:
+
+Input: values = [5,4,3,2,1], labels = [1,3,3,3,2], num_wanted = 3, use_limit = 2
+Output: 12
+Explanation: The subset chosen is the first, second, and third item.
+Example 3:
+
+Input: values = [9,8,8,7,6], labels = [0,0,0,1,1], num_wanted = 3, use_limit = 1
+Output: 16
+Explanation: The subset chosen is the first and fourth item.
+Example 4:
+
+Input: values = [9,8,8,7,6], labels = [0,0,0,1,1], num_wanted = 3, use_limit = 2
+Output: 24
+Explanation: The subset chosen is the first, second, and fourth item.
+
+*/
+
+
+/*
+ * @param {number[]} values
+ * @param {number[]} labels
+ * @param {number} num_wanted
+ * @param {number} use_limit
+ * @return {number}
+ */
+var largestValsFromLabels = function(values, labels, num_wanted, use_limit) {
+    const store = [];
+    const set = {};
+    let sum = 0;
+    for(let i = 0; i <labels.length; i++){
+        store[i] = [values[i],labels[i]];
+    };
+    store.sort((a, b) => b[0] - a[0]);
+    for(let num of store){
+        if(!num_wanted) return sum;
+        set[num[1]] = (set[num[1]] || 0) + 1;
+        if(set[num[1]] > use_limit) continue;
+        sum += num[0];
+        num_wanted -= 1;
+    }
+    return sum;
+};
